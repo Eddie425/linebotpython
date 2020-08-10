b5986e30241682d875d3871663373a3462ae6e94
Eddie Chang <eddie@StormChaserMBP.local>
2020-08-10 12:49:34 +0800

    fix bug

--- custom_models/call_database.py
+++ custom_models/call_database.py
@@ -2,30 +2,27 @@ import os
 import psycopg2
 
 
-class call_database:
-    """docstring for call_database."""
+def web_select_overall(self):
+    DATABASE_URL = os.environ['DATABASE_URL']
 
-    def web_select_overall(self):
-        DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    cursor = conn.cursor()
 
-        conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-        cursor = conn.cursor()
+    postgres_select_query = f"""SELECT * FROM user_account
+    ORDER BY last_login;"""
 
-        postgres_select_query = f"""SELECT * FROM user_account
-        ORDER BY last_login;"""
+    cursor.execute(postgres_select_query)
 
-        cursor.execute(postgres_select_query)
+    message = []
+    while True:
+        temp = cursor.fetchmany(10)
 
-        message = []
-        while True:
-            temp = cursor.fetchmany(10)
+        if temp:
+            message.extend(temp)
+        else:
+            break
 
-            if temp:
-                message.extend(temp)
-            else:
-                break
+    cursor.close()
+    conn.close()
 
-        cursor.close()
-        conn.close()
-
-        return message
+    return message
