9cf8c5ff2f9a9a099963d057481d8c92f969e56c
Eddie Chang <eddie@StormChaserMBP.local>
2020-09-20 16:21:38 +0800

    test

--- app_core.py
+++ app_core.py
@@ -94,9 +94,9 @@ def callBackTimeTreeApi():
     r = requests.get(url=URL)
 
     # extracting data in json format
-    data = r.json()
+    # data = r.json()
 
-    print(data)
+    print(r)
     # extracting latitude, longitude and formatted address
     # of the first matching location
     # latitude = data['results'][0]['geometry']['location']['lat']
