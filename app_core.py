4befde48eef8149500f375d3c1b2f432d9eccb42
echang1 <echang1@ftdm.com.tw>
2021-01-18 17:08:19 +0800

    test event

--- app_core.py
+++ app_core.py
@@ -11,6 +11,9 @@ import configparser
 import requests
 import json
 
+from timetree_sdk.models import Event, EventData, EventAttributes, EventRelationships, EventRelationshipsLabel, \
+    EventRelationshipsLabelData, EventRelationshipsAttendees, EventRelationshipsAttendeesData
+
 from custom_models import call_database
 
 app = Flask(__name__)
@@ -21,6 +24,7 @@ config.read('config.ini')
 
 line_bot_api = LineBotApi(config.get('line-bot', 'channel_access_token'))
 handler = WebhookHandler(config.get('line-bot', 'channel_secret'))
+csrf_token = "bEWJkgE2sU9-7FKEVIdItGhgw9bBGNf3wxkhHvv_moLJW9gw"
 
 
 # 接收 LINE 的資訊
@@ -69,6 +73,62 @@ def pixabay_isch(event):
                 TextSendMessage(text=event.timestamp)
             )
 
+        if event.message.text == "event":
+
+            client_id = "YSQLrHS4gy7nEPBQAOuYugsfDxb1UkLjV7Q5NkilEn8"
+            api = TimeTreeApi(csrf_token)
+
+            event = Event(
+                data=EventData(
+                    attributes=EventAttributes(
+                        title='Zoe Teacher',
+                        category='schedule',
+                        all_day=False,
+                        start_at='2021-01-27T11:00:00.000Z',
+                        end_at='2021-01-27T13:00:00.000Z',
+                        description='Description',
+                        location='Taipei',
+                        start_timezone='Asia/Taipei',
+                        end_timezone='Asia/Taipei'
+                    ),
+                    # relationships=EventRelationships(
+                    #     label=EventRelationshipsLabel(
+                    #         data=EventRelationshipsLabelData(
+                    #             id='LABEL_ID',
+                    #             type='label'
+                    #         )
+                    #     ),
+                    #     attendees=EventRelationshipsAttendees(
+                    #         data=[EventRelationshipsAttendeesData(
+                    #             id='USER_ID',
+                    #             type='user'
+                    #         )]
+                    #     )
+                    # )
+                )
+            )
+            response = api.create_event('zizBvYcXdFur', event)
+            print(response.data.attributes.title)  # Title
+
+            calendar = api.get_calendar('zizBvYcXdFur')
+            print(" Calendar = " + calendar.data.attributes.name)
+
+            events = api.get_upcoming_events('zizBvYcXdFur', 'Asia/Taiwan', 7)
+            print("title : " + events.data[0].attributes.title)
+            print("category : " + events.data[0].attributes.category)
+            print("all_day : " + str(events.data[0].attributes.all_day))
+            print("start_at : " + events.data[0].attributes.start_at)
+            print("start_timezone : " + events.data[0].attributes.start_timezone)
+            print("end_at : " + events.data[0].attributes.end_at)
+            print("end_timezone : " + events.data[0].attributes.end_timezone)
+            print("description : " + events.data[0].attributes.description)
+            print("location : " + events.data[0].attributes.location)
+            line_bot_api.reply_message(
+                event.reply_token,
+                TextSendMessage(text="Create Event OK~" + "  title : " + events.data[0].attributes.title)
+            )
+
+
         if "https://" in event.message.text:
             line_bot_api.reply_message(
                 event.reply_token,
@@ -82,8 +142,7 @@ def pixabay_isch(event):
 def callBackTimeTreeApi():
 
     redirect_uri = "https://lineboteddie.herokuapp.com/callBackTimeTreeApi"
-    csrf_token = "bEWJkgE2sU9-7FKEVIdItGhgw9bBGNf3wxkhHvv_moLJW9gw"
-    client_id = "YSQLrHS4gy7nEPBQAOuYugsfDxb1UkLjV7Q5NkilEn8"
+    # client_id = "YSQLrHS4gy7nEPBQAOuYugsfDxb1UkLjV7Q5NkilEn8"
     api = TimeTreeApi(csrf_token)
     calendar = api.get_calendar('zizBvYcXdFur')
 
